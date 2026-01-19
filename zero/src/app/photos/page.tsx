e081bacfc8e69eb2f4aa23bb23fb78ed2a0374f4
Than <thanzero7@gmail.com>
2026-01-19 16:16:32 +0700

    add new img

--- zero/src/app/photos/page.tsx
+++ zero/src/app/photos/page.tsx
@@ -27,6 +27,54 @@ export default function Photos() {
             className="rounded-lg shadow-lg w-full h-auto"
             unoptimized
           />
+          <Image 
+            src="https://i.pinimg.com/736x/d8/7b/fe/d87bfe94b75bc66c6d90229e56f68de1.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
+          <Image 
+            src="https://i.pinimg.com/736x/7e/72/28/7e72287f31f69b7ad0c25378b0d7f549.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
+          <Image 
+            src="https://i.pinimg.com/736x/cd/0c/b9/cd0cb955c6ee15776c6f51ccf3e5426e.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
+          <Image 
+            src="https://i.pinimg.com/736x/a2/43/5c/a2435c8a9fceca3d6679a05d5928faef.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
+          <Image 
+            src="https://i.pinimg.com/1200x/d9/0f/44/d90f44bdd5f878d69fd6ad3021c0b11f.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
+          <Image 
+            src="https://i.pinimg.com/736x/03/64/e8/0364e83560c3dc21a7e3fab45bc90854.jpg" 
+            alt="Zero Two" 
+            width={400} 
+            height={400} 
+            className="rounded-lg shadow-lg w-full h-auto"
+            unoptimized
+          />
         </div>
       </div>
     </div>
