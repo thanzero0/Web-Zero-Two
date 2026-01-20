5ff01432a562949b7d28677ddd31237d6d7aee2f
Than <thanzero7@gmail.com>
2026-01-20 10:03:08 +0700

    add new picture

--- zero/src/app/photos/page.tsx
+++ zero/src/app/photos/page.tsx
@@ -75,6 +75,207 @@ export default function Photos() {
         className="rounded-lg shadow-lg w-full"
         unoptimized
       />
+
+      <Image 
+        src="https://i.pinimg.com/736x/aa/87/cf/aa87cfbaeb3e9960cb39be5a02fe93ad.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/f0/ee/8d/f0ee8ddb6c9859999cb247a185f3ed66.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/17/01/06/170106c2be8f30c868ed298773ba9e50.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/82/74/0c/82740c247bb2ee2f73165646d9c28896.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/df/8b/22/df8b223adefdd7a59a09d0bc4b34263e.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/c9/7d/ca/c97dca1a28c65c1617f61cbe7e68439b.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/ca/01/52/ca0152d4a1e1a0a6f5225bc9ca2a33cf.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/474x/a6/ec/23/a6ec23ef9dcd2c6e9c15a7d9acbd443a.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/1200x/7b/bd/6d/7bbd6d2ae1231cf3f9c119d3282852aa.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/1200x/f7/84/bf/f784bf03d6ee2a4cb7ae2c3729378510.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/4a/b3/bb/4ab3bbdc1d50f2f989f20e4204e30f4e.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/e6/e3/a3/e6e3a343d3edadf34a4aaaaa2f39315a.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/1a/e5/e2/1ae5e277107638498ff8b5d1c02d051c.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/6a/2a/fe/6a2afe8c0541a4c9e5b95c232e64fcf7.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/f2/5b/74/f25b741da2101d92f5ff8ba1d163a7b5.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/b4/28/eb/b428eb758c9545d3b6736d54d879ceb9.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/01/d0/19/01d019f51774b79a8a13a3d5fccd5194.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/28/cf/61/28cf61d359423b73043e1f772d2c9721.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/bd/8f/bf/bd8fbfffae348f74c972b0e44a1d69ff.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/1200x/c4/3a/c4/c43ac4fdd7d7c18fa060d5689d1f0ca9.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/1200x/53/49/bb/5349bbe1f349e48a9f06c35bd905be23.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/f6/70/20/f670201bf0ce48d88e65bb8e0c76d90d.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/98/4b/38/984b38a06de6caa4822d7a9bcff30239.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/1200x/a8/cd/68/a8cd687352d37d5e8c54c5fb5e909569.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
+      <Image 
+        src="https://i.pinimg.com/736x/c4/4c/da/c44cda539c58be31e3f4155ca0f6fb9b.jpg" 
+        alt="Zero Two" 
+        width={400} 
+        height={600}
+        className="rounded-lg shadow-lg w-full"
+        unoptimized
+      />
 </div>
 
   );
